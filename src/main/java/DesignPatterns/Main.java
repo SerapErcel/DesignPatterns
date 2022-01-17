110dbf5ccbe641e9a7156bfe73d01b40e50d44f1
SERAP <ercelserap@gmail.com>
2022-01-18 00:44:08 +0300

    create factory pattern main

--- src/main/java/DesignPatterns/Main.java
+++ src/main/java/DesignPatterns/Main.java
@@ -5,6 +5,7 @@
  */
 package DesignPatterns;
 
+import DesignPatterns.CreationalDesignPatterns.Factory.*;
 import DesignPatterns.CreationalDesignPatterns.Singleton.Database;
 
 /**
@@ -17,9 +18,24 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        /* ------Singleton------
         Database firstInstanceRequest = Database.getInstance();
         Database secondInstanceRequest = Database.getInstance();
+         */
+        
+        ArabaFabrika bmw = new BmwFabrika();
+        ArabaFabrika audi = new AudiFabrika();
+        
+        for (final Araba araba:  bmw.getArabaListesi()) {
+            System.out.println(araba.getMarka() + " " + araba.getModel() + " , " 
+            + araba.getBeygirGucu());
+        }
+        for (final Araba araba:  audi.getArabaListesi()) {
+            System.out.println(araba.getMarka() + " " + araba.getModel() + " , " 
+            + araba.getBeygirGucu());
+        }
         
     }
-    
+
 }
